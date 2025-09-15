9ecb898f1a94b9665b772c065c64f645cace5a7b
Kobby Owen <kobby@nebula.io>
2025-09-15 20:17:20 +0000

    Used opython3

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
 
 exec gunicorn core.wsgi:application \
   --bind 0.0.0.0:${PORT:-8000} \
